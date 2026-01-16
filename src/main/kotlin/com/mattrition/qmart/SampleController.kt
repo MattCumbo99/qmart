fb150fdf76443ae430ef0d7d60e73e9f15007af7
Matthew Cumbo <mattcumbo99@gmail.com>
2026-01-15 18:10:20 -0600

    Simple controller

--- src/main/kotlin/com/mattrition/qmart/SampleController.kt
+++ src/main/kotlin/com/mattrition/qmart/SampleController.kt
@@ -1,10 +1,15 @@
 package com.mattrition.qmart
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SampleController {
-    @GetMapping("/sample")
-    fun sample(): String = "The sample has been called!"
+    @RequestMapping("/sample", method = [RequestMethod.GET])
+    @CrossOrigin
+    @ResponseBody
+    fun sample(): String = "The sample has been extracted!"
 }
